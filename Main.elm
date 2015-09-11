7aa6033596d77fd316803060b777667b6eb30a21
Pablo Botelho <pablo.bcb@gmail.com>
2015-09-11 15:24:04 -0300

    Update Main.elm

--- Main.elm
+++ Main.elm
@@ -3,9 +3,24 @@ import Counter exposing (update, view)
 import StartApp.Simple exposing (start)
 
 
+--main =
+  --start
+--    { model = 0
+--    , update = update
+--    , view = view
+--    }
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
 main =
-  start
-    { model = 0
-    , update = update
-    , view = view
-    }
+  table [ id "chessBoard", colspan 8, rowspan 8 ]
+        [ tr [] 
+          [ td [ id "A8" ] [text "BRENO"]
+          , td [ id "A7" ] [text "BRENO"]  
+          ]
+        , tr []
+          [ td [ id "A8" ] [text "BRENO"]
+          , td [ id "A7" ] [text "BRENO"]  
+          ]
+        ]
