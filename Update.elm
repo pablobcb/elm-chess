f7b332bbb1488999749374ea6afd4848eb196796
Pablo Lins <pablo.bcb@gmail.com>
2015-09-19 20:09:49 -0300

    separated files

---
+++ Update.elm
@@ -0,0 +1,14 @@
+module Update where 
+
+import Model exposing (..)
+
+type Action = Origin Position
+            | Destination Position
+            | Promotion Figure
+
+
+update : Action -> Game -> Game
+update action board =
+    case action of
+      Origin position ->
+        board
