8ee3faabfdb0f5dccf3c97e725e49ff6c573d7bf
Pablo Lins <pablo.bcb@gmail.com>
2015-09-19 21:44:46 -0300

    restart action added

--- Update.elm
+++ Update.elm
@@ -5,6 +5,7 @@ import Model exposing (..)
 type Action = Origin Position
             | Destination Position
             | Promotion Figure
+            | Restart
 
 
 update : Action -> Game -> Game
